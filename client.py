0e0c70a75d2501c02c13ec84ac4f9d5fe2d57251
Sachs <96741564+SachTheBased@users.noreply.github.com>
2022-04-16 06:53:46 -0500

    V1

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import websockets
 import asyncio
+import json
+from colorama import Fore as C
+
+with open('user.json', 'r') as f:
+    info = json.load(f)
 
 async def listen():
-    url = "ws://127.0.0.1:7890"
-    async with websockets.connect(url) as ws:
-        await ws.send("Hello Server!")
-        while True:
-            msg = await ws.recv()
-            print(msg)
+    url = "wss://Websocket.sachsthebased.repl.co"
+    while True:
+        message = input(f"{C.LIGHTBLUE_EX}Message: {C.RESET}")
+        async with websockets.connect(url) as ws:
+            await ws.send('{"Content": ' + '"' + message + '"' + ',"Token": ' + '"' + info['TOKEN'] + '"' + '}')
+
 asyncio.get_event_loop().run_until_complete(listen())
