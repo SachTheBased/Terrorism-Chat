6dc656cdd9c41cc03a9614abe959ffdc1ef0e76e
Sachs <96741564+SachTheBased@users.noreply.github.com>
2022-04-16 08:09:03 -0500

    Update client.py

--- Terrorism Chat V1/client.py
+++ Terrorism Chat V1/client.py
@@ -1,3 +1,4 @@
+#Only edit if you know what you're doing
 import websockets
 import asyncio
 import json
@@ -6,11 +7,31 @@ from colorama import Fore as C
 with open('user.json', 'r') as f:
     info = json.load(f)
 
-async def listen():
-    url = "wss://Websocket.sachsthebased.repl.co"
-    while True:
-        message = input(f"{C.LIGHTBLUE_EX}Message: {C.RESET}")
-        async with websockets.connect(url) as ws:
-            await ws.send('{"Content": ' + '"' + message + '"' + ',"Token": ' + '"' + info['TOKEN'] + '"' + '}')
+async def listen(room: int):
+    with open('user.json', 'r') as f:
+        info = json.load(f)
 
-asyncio.get_event_loop().run_until_complete(listen())
+    with open('user.json', 'w') as f:
+        info['ROOM'] = room
+        json.dump(info, f)
+
+    url = "wss://Websocket.sachsthebased.repl.co"
+
+    async with websockets.connect(url) as ws:
+        pass
+
+    while True:
+        message = input(f"{C.LIGHTGREEN_EX}Message: {C.RESET}")
+        async with websockets.connect(url) as ws:
+            await ws.send('{"Content": ' + '"' + message + '"' + ',"Token": ' + '"' + info['TOKEN'] + '"' + ',"Location": ' + str(room) + '}')
+
+while True:
+    print(f"""{C.LIGHTGREEN_EX}Options:
+[1] Connect to Room
+[2] View TOS{C.RESET}\n""")
+    option = int(input(f"{C.LIGHTGREEN_EX}Option: {C.RESET}"))
+
+    if option == 1:
+        room = int(input(f"{C.LIGHTGREEN_EX}Room ID: {C.RESET}"))
+        try: asyncio.get_event_loop().run_until_complete(listen(room))
+        except: print("Could not connect... Retrying")
