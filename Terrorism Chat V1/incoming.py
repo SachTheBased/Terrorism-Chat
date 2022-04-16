8ef686234c880a244c1fa4995c1df6b5ba6176fa
Sachs <96741564+SachTheBased@users.noreply.github.com>
2022-04-16 08:09:18 -0500

    Update incoming.py

--- Terrorism Chat V1/incoming.py
+++ Terrorism Chat V1/incoming.py
@@ -1,11 +1,19 @@
 import websockets
 import asyncio
+import json
 from colorama import Fore as C
 
 async def listen():
     url = "wss://Websocket.sachsthebased.repl.co"
     async with websockets.connect(url) as ws:
         while True:
+            with open('user.json', 'r') as f:
+                info = json.load(f)
             msg = await ws.recv()
-            print(C.LIGHTGREEN_EX + msg + C.RESET)
-asyncio.get_event_loop().run_until_complete(listen())
+            msg = json.loads(msg)
+            if msg['Location'] == int(info['ROOM']):
+                print(f"{C.LIGHTGREEN_EX}{msg['Author']}: {msg['Content']}{C.RESET}")
+
+while True:
+    try: asyncio.get_event_loop().run_until_complete(listen())
+    except: print("Could not connect... Retrying")
