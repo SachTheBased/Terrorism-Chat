0abce8c2128e4d7b6fa13bb269bd7843ed4ff372
Sachs <96741564+SachTheBased@users.noreply.github.com>
2022-04-16 06:54:37 -0500

    V2

--- server_echo_broadcast.py
+++ server_echo_broadcast.py
@@ -1,27 +1,40 @@
 import websockets
 import asyncio
+import socket
+import json
 
-PORT = 7890
+PORT = 1000
 print("Server listening on Port " + str(PORT))
 
 connected = set()
 
+async def history(websocket):
+    with open('messages.json', 'r') as f:
+        messages = json.load(f)
+    for message in messages["Messages"]:
+        await websocket.send(f"{message['Author']}: {message['Content']}")
+
 async def echo(websocket, path):
-    print("A client just connected")
-    # Store a copy of the connected client
     connected.add(websocket)
+    await history(websocket)
     try:
         async for message in websocket:
-            print("Received message from client: " + message)
-            # Send a response to all connected clients except sender
+            message = json.loads(message)
             for conn in connected:
                 if conn != websocket:
-                    await conn.send("Someone said: " + message)
+                  with open('users.json', 'r') as f:
+                    tokens = json.load(f)
+                  await conn.send(f"{tokens[message['Token']]['Username']}: {message['Content']}")
+                  with open('messages.json', 'r') as f:
+                    contents = json.load(f)
+                  with open('messages.json', 'w') as f:
+                    contents["Messages"].append({"Author": tokens[message['Token']]['Username'], "Content": message['Content']})
+                    json.dump(contents, f)
     except websockets.exceptions.ConnectionClosed as e:
         print("A client just disconnected")
     finally:
         connected.remove(websocket)
 
-start_server = websockets.serve(echo, "localhost", PORT)
+start_server = websockets.serve(echo, "0.0.0.0", PORT)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
